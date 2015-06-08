c1108fd067547da2a5a2327eaaf35d3ddb4cb56d
Youchen Lee <copyleft@utcr.org>
2015-06-08 18:07:49 +0800

    modify remove script

--- tools/remove-low-score-terms.php
+++ tools/remove-low-score-terms.php
@@ -7,16 +7,27 @@ if (array_key_exists(1, $argv)) {
 $redis = new Redis();
 
 $redis->connect('127.0.0.1', 6379);
-$key = 'CKIP:TERMS:' . $date;
-$count = $redis->zCard($key);
-$max_score_arr = $redis->zRevRange($key, 0, 0);
-$max_score = 'unknown';
-$max_score_term = 'unknown';
-if (count($max_score_arr) === 1) {
-	$max_score_term = $max_score_arr[0];
-	$max_score = $redis->zScore($key, $max_score_arr[0]);
+
+function delData($redis, $key) {
+
+    $count = $redis->zCard($key);
+    $max_score_arr = $redis->zRevRange($key, 0, 0);
+    $max_score = 'unknown';
+    $max_score_term = 'unknown';
+    if (count($max_score_arr) === 1) {
+        $max_score_term = $max_score_arr[0];
+        $max_score = $redis->zScore($key, $max_score_arr[0]);
+    }
+    fwrite(STDERR, " - key=|$key|, count=$count, max_score_term={$max_score_term}, max_score=$max_score\n");
+    $res = $redis->zRemRangeByScore($key, 0, 20);
+    fwrite(STDERR, "$res terms removed\n\n");
 }
-fwrite(STDERR, " - key=|$key|, count=$count, max_score_term={$max_score_term}, max_score=$max_score\n");
-$res = $redis->zRemRangeByScore($key, 0, 20);
-fwrite(STDERR, "$res terms removed\n\n");
+
+$key = 'CKIP:TERMS:' . $date;
+delData($redis, $key);
+foreach (array('0', '1', '2', '3') as $quarter) {
+    $key = 'CKIP:TERMS:' . $date . ':Q' . $quarter;
+    delData($redis, $key);
+}
+
 exit (0);
